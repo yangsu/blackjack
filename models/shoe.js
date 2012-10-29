964d2c639d378da0c98207dcf88a0a8036e18923
Yang Su <suyang1+services@gmail.com>
2012-10-29 17:17:33 -0400

    Removed commented out test code for shoes

--- models/shoe.js
+++ models/shoe.js
@@ -6,10 +6,6 @@ var _ = require('lodash')
 function Shoe(numberOfDecks, shuffleThreshold) {
   this.deck = Deck.standardDeck(numberOfDecks).shuffle();
 
-  // this.deck = new Deck(_.map(_.range(10), function () {
-  //   return new Card('Spades', 'A');
-  // }));
-
   // when the number of cards left in the deck <= threshold, the deck shuffles
   this.threshold = shuffleThreshold || config.defaultThreshold;
 }
